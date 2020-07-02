98a761454edfb55389045692e403b3eb8af116ae
ikakooo <i_chkhitunidze@cu.edu.ge>
2020-07-02 19:52:43 +0400

    init

--- app/src/main/java/com/example/movieapplication/bottom_navigation/ui/MovieNavigationActivity.kt
+++ app/src/main/java/com/example/movieapplication/bottom_navigation/ui/MovieNavigationActivity.kt
@@ -24,7 +24,7 @@ class MovieNavigationActivity : AppCompatActivity() {
                 R.id.navigation_home,
                 R.id.navigation_dashboard,
                 R.id.navigation_notifications,
-                R.id.navigation_notifications
+                R.id.navigation_search
 
             )
         )
