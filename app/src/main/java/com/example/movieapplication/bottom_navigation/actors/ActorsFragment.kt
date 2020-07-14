ee0a8836c96efa092989195343fd8b11b5488800
Your Name <dato.beriashvili@gmail.com>
2020-07-14 22:49:35 +0400

    commit

--- app/src/main/java/com/example/movieapplication/bottom_navigation/actors/ActorsFragment.kt
+++ app/src/main/java/com/example/movieapplication/bottom_navigation/actors/ActorsFragment.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -35,6 +36,7 @@ class ActorsFragment : Fragment() {
 
         actorsViewModel.popularActorsLiveDataModel.observe(viewLifecycleOwner, Observer{
             actorsList.addAll(it)
+            root.progressBarVisibilityID.isVisible = false
             actorsAdapter.notifyDataSetChanged()
         })
         return root
