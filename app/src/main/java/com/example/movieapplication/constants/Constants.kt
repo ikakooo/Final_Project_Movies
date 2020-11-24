cfa2b4d7b4064a5a5d3f60a9cda887953798ebdf
ikakooo <i_chkhitunidze@cu.edu.ge>
2020-11-25 00:16:43 +0400

    api key changed

--- app/src/main/java/com/example/movieapplication/constants/Constants.kt
+++ app/src/main/java/com/example/movieapplication/constants/Constants.kt
@@ -2,6 +2,7 @@ package com.example.movieapplication.constants
 
 object Constants {
     const val BASE_IMG_URL = "https://image.tmdb.org/t/p/w780/"
-    const val API_KEY = "22c64b1eb2954257036c84ed667c4109"
+   // const val API_KEY = "22c64b1eb2954257036c84ed667c4109"
+    const val API_KEY = "0e03d86efe00ea1a1e1dd7d2a4717ba1"//"22c64b1eb2954257036c84ed667c4109"
     const val BASE_URL_MOVIES = "https://api.themoviedb.org/"
 }
